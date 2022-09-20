8b571a996144837c166ad11ea1ca4960e1918be0
Carlos Zamora Sanz <32102276+Charlo270398@users.noreply.github.com>
2022-09-20 13:36:41 +0200

    Sort Get News List Fix

--- demo_carlos_backend/src/services/new/getNewsListService.js
+++ demo_carlos_backend/src/services/new/getNewsListService.js
@@ -2,7 +2,7 @@ const { NewModel } = require ("../../models/NewModel")
 
 async function getNewsListService(filter = {}) {
     try {
-        return await NewModel.find(filter).exec();
+        return await NewModel.find(filter).sort([['archiveDate', -1], ['date', -1]]).exec();
     } catch (error) {
         console.error(error)
     }

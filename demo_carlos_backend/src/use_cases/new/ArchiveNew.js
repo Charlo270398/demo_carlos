e894e87156eaf3447c64f88b493d9b871e06c4d7
Carlos Zamora Sanz <32102276+Charlo270398@users.noreply.github.com>
2022-09-20 13:36:56 +0200

    Variable name fixing

--- demo_carlos_backend/src/use_cases/new/ArchiveNew.js
+++ demo_carlos_backend/src/use_cases/new/ArchiveNew.js
@@ -5,7 +5,7 @@ async function ArchiveNew({_id}) {
         //Check _id param (not undefined and alphanumeric value)
         if(_id !== undefined && RegExp(/^[a-z0-9]+$/i).test(_id)){
             const archivedNew = await archiveNewService(_id);
-            return {codeResult: archiveNewService.modifiedCount === 1 ? 200 : 400}
+            return {codeResult: archivedNew.modifiedCount === 1 ? 200 : 400}
         } else {
             throw Error("Bad request: invalid _id value")
         }
